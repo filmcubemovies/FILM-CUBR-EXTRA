f8e6247bfecfd3e7fc06bfd6d1b7910775fa6379
filmcube <97111533+filmcube@users.noreply.github.com>
2022-03-07 15:18:07 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -41,7 +41,7 @@ async def save_group(bot, message):
                 InlineKeyboardButton('⚡ HΞLᎮ ⚡', url=f"https://t.me/{temp.U_NAME}?start=help")
             ],
             [
-                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez')
+                InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/+lthJys0pMMY4YWE1')
             ]
             ]
         reply_markup=InlineKeyboardMarkup(buttons)
