9b9a4a7b40b34d0da8ca7f775c28ca6843134f3d
filmcubemovies <101870460+filmcubemovies@users.noreply.github.com>
2022-06-20 20:19:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1346,14 +1346,14 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         cap = f"Here is what i found for your query {search}"
-    if imdb and imdb.get('https://telegra.ph/file/984c05c739f97fc355ba9.jpg'):
+    if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
-            pic = imdb.get('https://telegra.ph/file/984c05c739f97fc355ba9.jpg')
+            pic = imdb.get(poster')
             poster = pic.replace("https://telegra.ph/file/984c05c739f97fc355ba9.jpg")
-            await message.reply_photo(photo='https://telegra.ph/file/984c05c739f97fc355ba9.jpg', caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo= poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
