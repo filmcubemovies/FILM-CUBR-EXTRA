44f1df4c9ab8ce7f71af5ad8ee3a9b472d6cf1f2
filmcubemovies <101870460+filmcubemovies@users.noreply.github.com>
2022-06-20 20:21:47 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1353,7 +1353,7 @@ async def auto_filter(client, msg, spoll=False):
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get(poster')
             poster = pic.replace("https://telegra.ph/file/984c05c739f97fc355ba9.jpg")
-            await message.reply_photo(photo= poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=poster, caption=cap[:1024], reply_markup=InlineKeyboardMarkup(btn))
         except Exception as e:
             logger.exception(e)
             await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
